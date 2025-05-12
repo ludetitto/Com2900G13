853dd8412ba17db1ae2382c05a61fb22d5602b07
Lucia <ldetitto10@gmail.com>
2025-05-12 19:24:52 -0300

    Implementacion SQL dinámico para el ruteo de la creación de la base de datos.

--- 00_CreacionBaseDatos.sql
+++ 00_CreacionBaseDatos.sql
@@ -5,34 +5,50 @@
 -- Archivo: 00_CreacionBaseDatos.sql
 -- Descripción: Creación de la base de datos Com3900G13 con configuración ajustada y portable
 
+-- Se utiliza un mecanismo de SQL dinámico para poder insertar la ruta adecuada.
+DECLARE @dbName NVARCHAR(128) = 'Com3900G13';
+DECLARE @dataFile NVARCHAR(260); -- Suficientes caracteres para guardar la ruta.
+DECLARE @logFile NVARCHAR(260);
+DECLARE @dataPath NVARCHAR(260);
+DECLARE @sql NVARCHAR(MAX); -- Puede crecer hasta 2GB (máximo).
+
+-- Se obtiene la ruta del usuario
+SET @dataPath = CAST(SERVERPROPERTY('InstanceDefaultDataPath') AS NVARCHAR(260));
+SET @dataFile = @dataPath + @dbName + '_data.mdf';
+SET @logFile = @dataFile + @dbName + '_log.ldf';
+
 -- Validar si la base ya existe para evitar duplicados
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Com3900G13')
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @dbName)
 BEGIN
-    CREATE DATABASE Com3900G13
+    SET @sql = '
+	CREATE DATABASE [' + @dbName + ']
     ON PRIMARY
     (
-        NAME = 'Com3900G13_data',
+        NAME = ''' + @dbName + ''',
+		FILENAME = ''' + @dataFile + ''',
         SIZE = 20MB,              -- Tamaño inicial razonable para pruebas y demo
         FILEGROWTH = 5MB,         -- Controlado, evita microfragmentación
         MAXSIZE = 1024MB          -- Tope máximo según requerimiento de 1 GB en 2 años
     )
     LOG ON
     (
-        NAME = 'Com3900G13_log',
+        NAME = ''' + @dbName + '_log'',
+		FILENAME = ''' + @logFile + ''',
         SIZE = 10MB,
         FILEGROWTH = 2MB,
         MAXSIZE = 200MB
     );
 
     -- Ajustar modelo de recuperación si no se requiere backup en caliente
-    ALTER DATABASE Com3900G13 SET RECOVERY SIMPLE;
+    ALTER DATABASE [' + @dbName + '] SET RECOVERY SIMPLE;
+	';
+	EXEC(@sql);
 END
 ELSE
 BEGIN
     PRINT 'La base de datos Com3900G13 ya existe. No se creó nuevamente.';
 END;
-GO
 
 -- Usar la base de datos creada o existente
-USE Com3900G13;
+EXEC('USE [' + @dbName + ']');
 GO
