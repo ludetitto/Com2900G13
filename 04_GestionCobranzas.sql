a93f280ad6e3743f5388819236d33895f7316130
Lucia <ldetitto10@gmail.com>
2025-06-10 18:18:46 -0300

    Gestión de interés agregado por 1er vencimiento.

--- 04_GestionCobranzas.sql
+++ 04_GestionCobranzas.sql
@@ -8,6 +8,11 @@
             De Titto Lucia 46501934
 ========================================================================= */
 USE COM2900G13
+
+/*____________________________________________________________________
+  _________________________ RegistrarCobranza ________________________
+  ____________________________________________________________________*/
+
 IF OBJECT_ID('cobranzas.RegistrarCobranza', 'P') IS NOT NULL
     DROP PROCEDURE cobranzas.RegistrarCobranza;
 GO
@@ -121,7 +126,9 @@ BEGIN
 END;
 GO
 
---STORE PROCEDURE HABILITAR DEBITO AUTOMATICO
+/*____________________________________________________________________
+  _____________________ HabilitarDebitoAutomatico ____________________
+  ____________________________________________________________________*/
 
 IF OBJECT_ID('cobranzas.HabilitarDebitoAutomatico', 'P') IS NOT NULL
     DROP PROCEDURE cobranzas.HabilitarDebitoAutomatico;
@@ -151,7 +158,9 @@ BEGIN
 END;
 GO
 
---STORE PROCEDURE DESHABILITAR DEBITO AUTOMATICO
+/*____________________________________________________________________
+  ___________________ DeshabilitarDebitoAutomatico ___________________
+  ____________________________________________________________________*/
 
 IF OBJECT_ID('cobranzas.DeshabilitarDebitoAutomatico', 'P') IS NOT NULL
     DROP PROCEDURE cobranzas.DeshabilitarDebitoAutomatico;
@@ -200,16 +209,23 @@ BEGIN
 
     BEGIN TRY
         BEGIN TRANSACTION;
-
+		
+		DECLARE @montoPago DECIMAL(10,2);
         -- Validar existencia del pago
         IF NOT EXISTS (
-            SELECT 1 FROM cobranzas.Pago WHERE id_pago = @idPago
+            SELECT monto = @montoPago FROM cobranzas.Pago WHERE id_pago = @idPago
         )
         BEGIN
             RAISERROR('No existe un pago con el ID especificado.', 16, 1);
             ROLLBACK TRANSACTION;
             RETURN;
         END;
+
+		IF @monto > @montoPago
+		BEGIN
+			RAISERROR('El monto del reembolso no puede superar el monto del pago original.', 16, 1);
+			RETURN;
+		END;
 
         DECLARE @idSocio INT = (
             SELECT f.id_socio
@@ -393,7 +409,7 @@ BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
 
-        IF @id_socio <> NULL
+        IF @id_socio IS NOT NULL
         BEGIN
             EXEC cobranzas.RegistrarPagoACuenta
                 @idSocio = @id_socio,
@@ -537,5 +553,48 @@ BEGIN
         IF XACT_STATE() <> 0 ROLLBACK TRANSACTION;
         THROW;
     END CATCH
+END;
+GO
+
+/*____________________________________________________________________
+  ____________________ AplicarRecargoVencimiento _____________________
+  ____________________________________________________________________*/
+
+  IF OBJECT_ID('cobranzas.AplicarRecargoVencimiento', 'P') IS NOT NULL
+    DROP PROCEDURE cobranzas.AplicarRecargoVencimiento;
+GO
+
+CREATE PROCEDURE cobranzas.AplicarRecargoVencimiento
+	@descripcion_recargo VARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @porcentaje_recargo DECIMAL(5, 2);
+
+    -- Obtener el porcentaje del recargo
+    SELECT @porcentaje_recargo = porcentaje
+    FROM facturacion.Recargo
+    WHERE descripcion = @descripcion_recargo
+      AND vigencia > GETDATE();
+
+    -- Verificar si se encontró el recargo
+    IF @porcentaje_recargo IS NOT NULL
+    BEGIN
+        UPDATE facturacion.Factura
+        SET monto_total = monto_total * (1 + @porcentaje_recargo)
+        WHERE anulada = 0 
+        AND id_factura IN (SELECT F.id_factura
+						   FROM facturacion.Factura F
+						   INNER JOIN facturacion.DetalleFactura D ON D.id_factura = F.id_factura
+						   WHERE D.tipo_item <> 'Actividad Extra'
+						   AND F.fecha_vencimiento1 < GETDATE() 
+						   AND F.fecha_vencimiento2 > GETDATE()
+						   );
+    END
+    ELSE
+    BEGIN
+        RAISERROR('No se encontró un recargo válido con la descripción proporcionada.', 16, 1);
+    END
 END;
 GO
